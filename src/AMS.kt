e73f8e0c6de1542068530218d7f04bf49fd521bd
Johan Böhlin <johboh@gmail.com>
2018-08-25 11:59:07 +0200

    stuff

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 fun main(args: Array<String>) {
     assert(false, canAddFish(10.0, listOf(3,3,3)));
     assert(true, canAddFish(8.0, listOf(2,2,2), hasDecorations = false));
@@ -6,6 +8,18 @@ fun main(args: Array<String>) {
 
     println(whatShouldIDoToday("happy"))
     println(whatShouldIDoToday("angry"))
+    println(funWithWhen(20, "no"))
+
+    val dirty = {dirty : Int -> dirty > 30}
+    println(dirty(30))
+
+    println(checkOk("OK", isOk))
+    println(checkOk("OK") { ok -> if (ok == "OK") 999 else 111 })
+}
+
+val isOk: (String) -> (Int) = { ok -> if (ok == "OK") 666 else 333  }
+fun checkOk(isThisOk : String, operation : (String) -> (Int)) : Int {
+    return operation(isThisOk)
 }
 
 fun canAddFish(tankSize : Double, currentFish: List<Int>, fishSize : Int = 2, hasDecorations : Boolean = true) : Boolean {
@@ -21,3 +35,13 @@ fun whatShouldIDoToday(mood : String) = when (mood) {
     "happy" -> "Go out and have fun"
     else -> "Stay home"
 }
+
+fun funWithWhen(number : Int, lucky : String) : String {
+    return when {
+        isLucky(number) -> "Its your lucky number"
+        lucky == "I'm lucky" -> "You are lucky"
+        else -> "No luck today."
+    }
+}
+
+fun isLucky(lucky : Int) = lucky in 1..30
