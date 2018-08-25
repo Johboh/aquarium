943a16bf19c8408d5572762792a4c39636a56888
Johan Böhlin <johboh@gmail.com>
2018-08-25 11:20:30 +0200

    when

--- src/AMS.kt
+++ src/AMS.kt
@@ -3,16 +3,21 @@ fun main(args: Array<String>) {
     assert(true, canAddFish(8.0, listOf(2,2,2), hasDecorations = false));
     assert(false, canAddFish(9.0, listOf(1,1,3), 3));
     assert(true, canAddFish(10.0, listOf(), 7, true));
+
+    println(whatShouldIDoToday("happy"))
+    println(whatShouldIDoToday("angry"))
 }
 
 fun canAddFish(tankSize : Double, currentFish: List<Int>, fishSize : Int = 2, hasDecorations : Boolean = true) : Boolean {
-    var totalSize : Double = fishSize.toDouble()
-    for (fish in currentFish) {
-        totalSize += fish
-    }
+    val totalSize : Double = fishSize.toDouble() + currentFish.sum()
     return totalSize <= tankSize * (if (hasDecorations) 0.8 else 1.0);
 }
 
 fun assert(expected : Boolean, actual: Boolean) {
     println("Test did ${if (expected != actual) "not" else ""} pass")
+}
+
+fun whatShouldIDoToday(mood : String) = when (mood) {
+    "happy" -> "Go out and have fun"
+    else -> "Stay home"
 }
